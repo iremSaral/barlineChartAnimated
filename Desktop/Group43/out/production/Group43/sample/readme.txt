adcbc2a957f4b80f7a4320bd4eaef5df09a4051a
iremsaral <1030510058@erciyes.edu.tr>
2023-01-26 16:16:00 +0300

    Project's finished

---
+++ Desktop/Group43/out/production/Group43/sample/readme.txt
@@ -0,0 +1,1 @@
+We can use Amazon Corretto version 11.0.11 wwhen we are coding our program.
---
+++ Desktop/Group43/src/module-info.java
@@ -0,0 +1,9 @@
+module Group43 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires java.xml;
+    requires java.desktop;
+    opens sample;
+}
