0ac2ca1b9f08a6a2b178e4a1f89663c4f88589fc
iremsaral <1030510058@erciyes.edu.tr>
2023-03-12 21:23:03 +0300

    Operators

---
+++ Desktop/C#101/Operators/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama ve İşlemli Atama
+           /*
+            int d=8;
+            d=d*4;
+            Console.WriteLine(d);
+            d+=2;
+            Console.WriteLine(d);
+            d/=2;
+            Console.WriteLine(d);
+        */
+        //Mantıksal Operatorler //,&&,!
+      /*  bool isSuccess=true;
+        bool isCompleted=false;
+        if(isSuccess && isCompleted){
+            Console.WriteLine("And");
+        }
+        if(isSuccess ||isCompleted){
+            Console.WriteLine("OR");
+        }
+        if(!isCompleted)
+        {
+              Console.WriteLine("Not")  ;
+        }*/
+        /*//İlişkisel Operatorler
+        int a=3;
+        int b=4;
+        bool sonuc= a<b;
+        Console.WriteLine(sonuc);
+       sonuc= a>=b;
+       Console.WriteLine(sonuc);
+       sonuc= a==b;
+       Console.WriteLine(sonuc);
+       sonuc= a!=b ;
+       Console.WriteLine(sonuc);
+        */
+        //Aritmetik Operatorler
+         int sayi1=10;
+         int sayi2=5;
+         int sonuc1=sayi1/sayi2;
+         Console.WriteLine(sonuc1);
+         sonuc1=sayi1*sayi2;
+         Console.WriteLine(sonuc1);
+         sonuc1=sayi2++;
+         Console.WriteLine(sonuc1);
+
+         // % mod alma
+        Console.WriteLine(20%3);
+        }
+    }
+}
